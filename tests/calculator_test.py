37c85a043429cba9946b75c6b954255337c095bc
sravyamyla <sm465@njit.edu>
2021-11-02 02:37:17 -0400

    la

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -35,9 +35,3 @@ def test_calculator_division():
     calc = Calculator()
     result = calc.divide_numbers(10, 2)
     assert result == 5
-
-def test_calculator_division():
-        """ Testing division of two numbers"""
-        calc = Calculator()
-        result = calc.divide_numbers(0, 5)
-        assert result == 0
